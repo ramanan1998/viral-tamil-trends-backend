24ebee8f056ddfd9c41df2b97c005e350ca4458e
ramanan1998 <ramananr2698095@gmail.com>
2025-12-03 12:22:53 +0530

    mongo uri added

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import generateRoutes from "./routes/generate";
 import authRoutes from "./routes/auth";
 import userRoutes from "./routes/user";
 
-mongoose.connect(process.env.MONGO_URI as string).then(() => console.log("DB connected successfully")).catch(err => console.log(err));
+mongoose.connect("mongodb+srv://blackwinstech:uXtCZ9Bd6ZJtGKoX@blackwins-cluster.u9rwchf.mongodb.net/tamiltrends").then(() => console.log("DB connected successfully")).catch(err => console.log(err));
 
 const PORT = 5000;
 const app = express();
